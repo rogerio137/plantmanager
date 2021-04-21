575e798dfdf57c2fb690e375d8548196c60c7605
Rogerio <barrucalleortiz@gmail.com>
2021-04-21 11:21:51 -0300

    Aula 2 NLW

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,36 +1,43 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet, Text, TouchableOpacityProps } from 'react-native';
-import colors from '../styles/colors';
+import {
+    StyleSheet,
+    TouchableOpacity,
+    Text,
+    TouchableOpacityProps
+}from 'react-native';
+
+
+import colors  from '../styles/colors';
+import fonts from '../styles/fonts';
 
 interface ButtonProps extends TouchableOpacityProps{
     title: string;
 }
 
-export function Button({ title, ... rest } : ButtonProps){
-    return(
-        <TouchableOpacity   style={styles.button} 
-                            activeOpacity={0.7}
-                            {...rest}>
-                <Text style={styles.buttonText}>
-                    { title }
-                </Text>
-            </TouchableOpacity>
+export function Button({ title, ... rest }: ButtonProps){
+    return (
+        <TouchableOpacity style={styles.container}>
+            <Text 
+                style={styles.text}
+                {... rest}
+            >
+                {title}
+            </Text>
+        </TouchableOpacity>
     )
-}
+};
 
 const styles = StyleSheet.create({
-  
-    button: {
+    container :{
         backgroundColor: colors.green,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderRadius: 16,
-        marginBottom: 10,
         height: 56,
-        width: 56
+        borderRadius: 16,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-   
-    buttonText: {
-        color: colors.white
+    text:{
+        fontSize: 16,
+        color: colors.white,
+        fontFamily: fonts.heading
     }
 });
